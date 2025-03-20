bf36074abcb909a4169fb1ca4f468fbb8ba4238e
Frendzlu <mateuszfrancik.mtf@gmail.com>
2025-03-20 10:14:38 +0100

    Cleanup, add .gitignore

--- Lexer.py
+++ Lexer.py
@@ -1,30 +1,29 @@
 import pprint
-import sys
 
 
 class Scanner:
-    keepWhitespace = False
+    keep_whitespace = False
     pos = 0
     line = 0
-    tokenDict = {}
+    token_dict = {}
 
-    def __init__(self, tokenDict, keepWhitespace=False):
-        for key in tokenDict:
-            for token in tokenDict[key]:
-                self.tokenDict[token] = key
-        self.keepWhitespace = keepWhitespace
+    def __init__(self, token_dict, keep_whitespace=False):
+        for key in token_dict:
+            for token in token_dict[key]:
+                self.token_dict[token] = key
+        self.keep_whitespace = keep_whitespace
 
     def parse_file(self, filename):
         pass
 
     def parse_line(self, text):
-        if not self.keepWhitespace:
+        if not self.keep_whitespace:
             text = self.strip_whitespace(text)
         scanning_result = []
         errors = []
         for char in text:
-            if char in self.tokenDict:
-                scanning_result.append((self.tokenDict[char], char))
+            if char in self.token_dict:
+                scanning_result.append((self.token_dict[char], char))
             else:
                 errors.append("Unknown token at position {}".format(self.pos))
             self.pos += 1
@@ -34,6 +33,7 @@ class Scanner:
 
     def strip_whitespace(self, text):
         return text.replace(' ', '').replace('\n', '').replace('\r', '').replace('\t', '')
+
 
 if __name__ == "__main__":
     sc = Scanner({
@@ -46,5 +46,3 @@ if __name__ == "__main__":
     scres, err = sc.parse_line("3+3*(25-43)         costamcostam26+252")
     pprint.pprint(scres)
     pprint.pprint(err)
-
-
