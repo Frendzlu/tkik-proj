cf9eb2251e548935e75ba32e2db632ed14657af4
Frendzlu <mateuszfrancik.mtf@gmail.com>
2025-04-28 21:12:42 +0200

    working lark?

--- chord_notation_interpreter/grammar.py
+++ chord_notation_interpreter/grammar.py
@@ -4,10 +4,10 @@ from lark import Lark
 
 if __name__ == "__main__":
     grammar = reader("./grammar.txt")
-    chord_code = reader("./test.txt")
+    chord_code = reader("./cc.txt")
 
     # chord code parser
-    ccp = Lark(rf"{grammar}", start="start")
+    ccp = Lark(rf"{grammar}", start="start", parser='lalr')
 
     # abstract syntax tree
     ast = ccp.parse(chord_code)
